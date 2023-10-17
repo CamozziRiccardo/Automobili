a69c58dcb05a7f49569f4b30e100552a7fa51153
Camozzi.20185 <Camozzi.20185@E2-9-PC01>
2023-10-17 12:34:36 +0200

    Creazione classe con attributi e relativi se e get

--- Automobili/Class1.cs
+++ Automobili/Class1.cs
@@ -8,5 +8,38 @@ namespace Automobili
 {
     internal class Auto_Man
     {
+        //da 0 a 100, 6 marce, valorre 0 per la retro
+        protected int _vel;
+
+        //6 marce diverse
+        protected int _marcia;
+
+        //booleano per verificare l'accensione
+        protected bool _acc;
+
+        public Auto_Man() 
+        {
+            Vel = 0;
+            Marcia = 1;
+            Acc = false;
+        }
+
+        int Vel
+        {
+            get { return _vel; }
+            set { _vel = value; }
+        }
+
+        int Marcia
+        {
+            get { return _marcia; }
+            set { _marcia = value; }
+        }
+
+        bool Acc
+        {
+            get { return _acc; }
+            set { _acc = value; }
+        }
     }
 }
